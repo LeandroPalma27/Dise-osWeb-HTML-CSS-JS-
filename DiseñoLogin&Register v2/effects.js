4ff1b96469ee09534675e9300a0190fefd4b98e1
Leandro <psychomusicbox@gmail.com>
2021-11-28 21:52:49 -0500

    Login y register terminado (solo maqueta)

--- DiseñoLogin&Register v2/effects.js
+++ DiseñoLogin&Register v2/effects.js
@@ -1,16 +1,51 @@
+let divPrincipal = document.querySelector('.container-form');
 let divLogin = document.querySelector('.form_login');
 let divRegister = document.querySelector('.form_register');
-let btnRegisterActive = document.querySelector('.register-button');
-let btnLoginActive = document.querySelector('.login-button');
+let btnRegisterActive = document.querySelector('.goToCreateUser');
+let btnLoginActive = document.querySelector('.goToLogin');
+
+//Contenedores de las partes del formulario:
+let formTitle = document.querySelectorAll('.form_container-title');
+let formInputs = document.querySelectorAll('.form_container-inputs');
+let formButtons = document.querySelectorAll('.form_container-buttons');
+
+let inputsForm = document.querySelectorAll('.input-form');
+
 
 btnRegisterActive.addEventListener('click', function (e) {
+    divPrincipal.classList.toggle('active');
     divRegister.classList.toggle('active');
     divLogin.classList.toggle('active');
+    formTitle.forEach((e) => {
+        e.classList.toggle('active');
+    });
+    formInputs.forEach((e) => {
+        e.classList.toggle('active');
+    });
+    formButtons.forEach((e) => {
+        e.classList.toggle('active');
+    });
+    inputsForm.forEach((e) => {
+        e.classList.toggle('active');
+    })
 });
 
 btnLoginActive.addEventListener('click', function (e) {
+    divPrincipal.classList.toggle('active');
     divRegister.classList.toggle('active');
     divLogin.classList.toggle('active');
+    formTitle.forEach((e) => {
+        e.classList.toggle('active');
+    });
+    formInputs.forEach((e) => {
+        e.classList.toggle('active');
+    });
+    formButtons.forEach((e) => {
+        e.classList.toggle('active');
+    });
+    inputsForm.forEach((e) => {
+        e.classList.toggle('active');
+    })
 });
 
 
